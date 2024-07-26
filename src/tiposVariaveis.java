a6f910aedd1365d29d333f79808949c087ccd13d
Thalys Araujo <86092245+hucktails@users.noreply.github.com>
2024-07-25 23:57:25 -0300

    Add files via upload
    adição de diretorios para projeto java da DIO

---
+++ src/tiposVariaveis.java
@@ -0,0 +1,15 @@
+// Primeiro arquivo .java    😊
+public class tiposVariaveis {
+
+public static void main (String [] args) {
+// a variável precisa ter 'Tipo NomeBemDefinido = Atribuição (Em alguns casos)'
+    String botao = ("teu pai");
+
+    System.out.print ("Hello world!! It's me again.");
+
+    
+}
+
+}
+
+
